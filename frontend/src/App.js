2880effa59a271639a207ec0266617a3e223cb32
Akito Kasai <a.kasai415@gmail.com>
2017-05-09 15:02:33 +0900

    some error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
   render() {
     return (
       <h1>Hello, world.</h1>
     );
+  }
+}
+
+const fooApp extends Component {
+  render () {
+    return(
+          <p>error</p>
+        );
   }
 }
